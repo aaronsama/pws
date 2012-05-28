6fc4a13867c32104aeff64034b0a4710ed814fa8
Jan Lelis <mail@janlelis.de>
2012-05-28 23:38:38 +0200

    use less iterations in aruba

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,17 +12,19 @@ ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File:
 # Hooks
 
 BEGIN{
-  $original_pws_file = ENV["PWS"]
+  $original_pws_file   = ENV["PWS"]
+  $original_iterations = ENV["PWS_ITERATIONS"]
+  ENV["PWS_ITERATIONS"] = "2"
 }
 
 END{
   Clipboard.clear
   ENV["PWS"] = $original_pws_file
+  ENV["PWS_ITERATIONS"] = $original_iterations
   FileUtils.rm Dir['pws-test-*']
 }
 
 Around do |_, block|
-  # NOTE: You cannot parallelize the tests, because they use the clipboard and the env vars...
   Clipboard.clear
   ENV["PWS"] = File.expand_path('pws-test-' + SecureRandom.uuid)
   
@@ -35,7 +37,7 @@ end
 
 Before('@slow-hack') do
   @aruba_io_wait_seconds = 4.5
-  @aruba_timeout_seconds = 4.5
+  @aruba_timeout_seconds = 5
 end
 
 Before('@very-slow-hack') do
